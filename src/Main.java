7c813043df02bc22bdfdec686296f322d7a658a9
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-02-08 19:31:09 +0300

    Here I raised the value of the variables

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,19 @@ public class Main {
     public static void main(String[] args) {
         var dog = 8.0;
         System.out.println(dog);
+        dog = dog + 4;
+        System.out.println(dog);
+        System.out.println();
+        System.out.println();
         var cat = 3.6;
         System.out.println(cat);
+        cat = cat + 4;
+        System.out.println(cat);
+        System.out.println();
+        System.out.println();
         var paper = 763789;
+        System.out.println(paper);
+        paper = paper + 4;
         System.out.println(paper);
     }
 }
